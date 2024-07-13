9c5b4a480897ecaf0a7d36ea786260da4ecd5c34
=Winston bravo <wbravoanoni@gmail.com>
2024-07-13 00:04:46 -0400

    evento input seach eventos

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,23 @@
+import {useState} from "react"
+
 const Navbar = () => {
+
+    const [search, setSearch] = useState('')
+
+    const handleInputChange = (event) => {
+        setSearch(event.target.value);
+    }
+
+    console.log(search);
 
     return (
         <div>
             <p>Mi Boletera</p>
-            <input placeholder="Busca tu evento favorito"></input>   
+            <input 
+            placeholder="Busca tu evento favorito" 
+            onChange={handleInputChange}
+            value={search}
+            ></input>   
         </div>
     ) 
 
